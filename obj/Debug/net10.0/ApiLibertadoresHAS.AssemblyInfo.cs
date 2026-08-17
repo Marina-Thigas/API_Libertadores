14c5782908b9c036ef4557fe451b0a6941973a18
Marina-Thigas <maomenosetec@gmail.com>
2026-08-17 09:11:08 -0300

    aula 17/08

--- obj/Debug/net10.0/ApiLibertadoresHAS.AssemblyInfo.cs
+++ obj/Debug/net10.0/ApiLibertadoresHAS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiLibertadoresHAS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e92e5c24607c66b7415801d44d6287a126487c06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9242b83f360ac6048d82f8b6233471fe287a8cd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiLibertadoresHAS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiLibertadoresHAS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
